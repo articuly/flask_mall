5619e2c33b5692290ac19c56004808df6c1d7f82
articuly <articuly@gmail.com>
2020-09-28 22:50:05 +0800

    有效迁移数据库，增加地区字段

--- xp_mall/models/area.py
+++ xp_mall/models/area.py
@@ -5,11 +5,11 @@ from xp_mall.extensions import db
 
 # todo:在物流信息加入地区地址
 class Area(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    place = db.Column(db.String(50))
-    order_id = db.Column(db.Integer, default=0)
-    parent_id = db.Column(db.Integer, db.ForeignKey('area.id'), default=None)
+    place_id = db.Column(db.Integer, primary_key=True)
+    place_name = db.Column(db.String(50))
+    place_order = db.Column(db.Integer, default=0)
+    parent_id = db.Column(db.Integer, db.ForeignKey('area.place_id'), default=None)
 
-    parent = db.relationship('Area', back_populates="sub_cates", remote_side=[id])
-    sub_cates = db.relationship('Area', back_populates="parent", cascade='all, delete-orphan',
-                                order_by=order_id)
+    up_place = db.relationship('Area', back_populates="sub_place", remote_side=[place_id])
+    sub_place = db.relationship('Area', back_populates="up_place", cascade='all, delete-orphan',
+                                order_by=place_order)
