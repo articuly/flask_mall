f543746bffd19b525de39f141d62eb2052f121b7
articuly <articuly@gmail.com>
2020-10-02 23:50:45 +0800

    更换商城模板，完成管理后台、登陆注册、商品详情大部分内容

--- static/js/jquery.plugin/upload/up.php
+++
@@ -1,4 +0,0 @@
-<?php
-move_uploaded_file($_FILES["up_file"]["tmp_name"], "upload/" . $_FILES["up_file"]["name"]);
-header("Access-Control-Allow-Origin:*");
-echo json_encode(['img' => 'http://localhost/upload/'.$_FILES["up_file"]["name"]]);
